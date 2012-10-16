a8cd4e86ce50f0821c154b90f3d708f51e594a5f
Takashi Honda <takashi.0628.honda@gmail.com>
2012-10-16 21:40:45 +0900

    test

--- profile.php
+++ profile.php
@@ -3,6 +3,7 @@ Class UserProfile{
     public $sns = '';
     public $users = array(
         'user_id' => '',
+        'user_name' => ''.
         'user_icon' => '',
         'user_page' => ''
     );
