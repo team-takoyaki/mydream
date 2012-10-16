21857db34bbfe0e157f0f4eccf65c5e62089b0ab
Takashi Honda <takashi.0628.honda@gmail.com>
2012-10-16 22:05:34 +0900

    test

--- profile.php
+++ profile.php
@@ -9,23 +9,23 @@ Class UserProfile {
     );
 
     function add_sns($user_sns) {
-        $this->$sns = $user_sns;
+        $this->sns = $user_sns;
     }
 
     function add_user_id($user_id) {
-        $this->$users['user_id'] = $user_id;
+        $this->users['user_id'] = $user_id;
     }
 
     function add_user_name($user_name) {
-        $this->$users['user_name'] = $user_name;
+        $this->users['user_name'] = $user_name;
     }
 
     function add_user_icon($user_icon) {
-        $this->$users['user_icon'] = $user_icon;
+        $this->users['user_icon'] = $user_icon;
     }
 
     function add_user_page($user_page) {
-        $this->$users['user_page'] = $user_page;
+        $this->users['user_page'] = $user_page;
     }
 
     function get_users() {
