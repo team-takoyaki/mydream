252a0cf6c2146eb5b314ef8d0bc1ad1b74564e85
Takashi Honda <takashi.0628.honda@gmail.com>
2012-10-16 20:28:45 +0900

    fix bug

--- top.php
+++ top.php
@@ -3,5 +3,5 @@ session_start();
 if (isset($_SESSION['user_name'])) {
     include_once('tmpl/top.html.php');
 } else {
-    header('Location:http://team-takoyaki.com/dream');
+    header('Location:' . BASE_URL);
 }
