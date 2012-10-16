bcb1439371ac3a53d48a76bc78412d3c3481d1a3
Takashi Honda <takashi.0628.honda@gmail.com>
2012-10-16 21:45:52 +0900

    test

--- top.php
+++ top.php
@@ -3,7 +3,7 @@ require_once('profile.php');
 session_start();
 $up = new UserProfile();
 var_dump($_SESSION['up']);
-if (isset($_SESSION['user_name'])) {
+if (isset($_SESSION['up']->users['user_name'])) {
     include_once('tmpl/top.html.php');
 } else {
     header('Location:' . BASE_URL);
