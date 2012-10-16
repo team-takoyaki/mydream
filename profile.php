e86b2654a905fb4c81fab7a6b61dbe20cfa9ba15
Takashi Honda <takashi.0628.honda@gmail.com>
2012-10-16 22:03:24 +0900

    test

--- profile.php
+++ profile.php
@@ -1,34 +1,34 @@
 <?php
 Class UserProfile {
-    $sns = '';
-    $users = array(
+    var $sns = '';
+    var $users = array(
         'user_id' => '',
         'user_name' => '',
         'user_icon' => '',
         'user_page' => ''
     );
 
-    public add_sns($user_sns) {
-        $sns = $user_sns;
+    function add_sns($user_sns) {
+        $this->$sns = $user_sns;
     }
 
-    public add_user_id($user_id) {
-        $users['user_id'] = $user_id;
+    function add_user_id($user_id) {
+        $this->$users['user_id'] = $user_id;
     }
 
-    public add_user_name($user_name) {
-        $users['user_name'] = $user_name;
+    function add_user_name($user_name) {
+        $this->$users['user_name'] = $user_name;
     }
 
-    public add_user_icon($user_icon) {
-        $users['user_icon'] = $user_icon;
+    function add_user_icon($user_icon) {
+        $this->$users['user_icon'] = $user_icon;
     }
 
-    public add_user_name($user_page) {
-        $users['user_page'] = $user_page;
+    function add_user_name($user_page) {
+        $this->$users['user_page'] = $user_page;
     }
 
-    public get_users() {
+    function  get_users() {
         return $users;
     }
 }
