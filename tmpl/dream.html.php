221f97d5c6b8749d5120ad8091235e0f2372692a
Kashima Takumi <kodokurider@gmail.com>
2012-10-24 23:35:49 +0900

    change dreeam design

--- tmpl/dream.html.php
+++ tmpl/dream.html.php
@@ -1,37 +1,79 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
-    <meta charset="utf-8">
-    <title><?= $dream['title']; ?></title>
-    <link rel="stylesheet" type="text/css" href="css/write_dream.css">
-</head>
-<body>
-    <h1><?= htmlspecialchars($dream['title']); ?></h1>
-    <p>body <?= set_linked_from_text($dream['body']); ?></p>
-    <p>By <?= htmlspecialchars($dream['user_name']); ?></p>
-    <?php if ($is_my_dream === false) {?>
-    <form method="POST">
-        <p><button name="cheers" value="cheers">Cheers</button></p>
-        <?php if ($cheer_users > 0) { ?>
-        This post cheer by <?= htmlspecialchars($cheer_users) ?> users;
-        <?php } ?>
-    </form>
-    <?php }?>
-    <?php foreach ($comments as $comment) { ?>
-    <div class="comment">
-        <p><?= set_linked_from_text($comment['body']); ?></p>
-        <p><?= htmlspecialchars($comment['user_name']); ?></p>
-         <form method="POST">
-            <p><button name="comment_id" value="<?= $comment['id']; ?>"<?php display_button($is_thank[$comment['id']]);?>>Thanks</button></p>
-            <?php if (intval($thank_users[$comment['id']]['count']) > 0) { ?>
-            This post thanks by <?= htmlspecialchars($thank_users[$comment['id']]['count']); ?> users;
-            <?php } ?>
-         </form>
-    </div>
-    <?php } ?>
-    <form method="POST">
-        <textarea name="user_comment"></textarea>
-        <button name="post">投稿</button>
-    </form>
-</body>
+    <head>
+        <meta charset="utf-8">
+        <title><?= $dream['title']; ?></title>
+        <link rel="stylesheet" type="text/css" href="css/dream.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    </head>
+    <body>
+        <div id="body">
+            <a href="<?= BASE_URL ?>">
+                <header>
+                    <h1><?= $user_info['user_name'];?>の夢リスト</h1>
+                    <div class="user_image">
+                        <img src="<?= $user_info['user_image'];?>">
+                    </div>
+                </header>
+            </a>
+            <div class="dream_wrapper">
+                <div class="dream">
+                    <div class="dream_inner">
+                        <h1><?= htmlspecialchars($dream['title']); ?></h1>
+                        <p class="dream_body">body <?= set_linked_from_text($dream['body']); ?></p>
+                        <div class="author_wrapper"><span class="author"><?= htmlspecialchars($dream['user_name']); ?></span></div>
+                        <?php if ($is_my_dream === false) {?>
+                        <form method="POST" class="cheers_btn_form">
+                            <button name="cheers" value="cheers" class="cheers_btn">応援する</button>
+                            <?php if ($cheer_users > 0) { ?>
+                            <p class="cheers_message"><?= htmlspecialchars($cheer_users) ?>が応援しています</p>
+                            <?php } ?>
+                        </form>
+                    <?php }?>
+                    </div>
+                    <?php foreach ($comments as $comment) { ?>
+                    <div class="comment">
+                        <p class="comment_body"><?= set_linked_from_text($comment['body']); ?></p>
+                        <div class="comment_author"><?= htmlspecialchars($comment['user_name']); ?></div>
+                        <form method="POST" class="thanks_btn_form">
+                            <button name="comment_id" class="thanks_btn" value="<?= $comment['id']; ?>"<?php display_button($is_thank[$comment['id']]);?>>ありがとう</button>
+                            <?php if (intval($thank_users[$comment['id']]['count']) > 0) { ?>
+                            This post thanks by <?= htmlspecialchars($thank_users[$comment['id']]['count']); ?> users;
+                            <?php } ?>
+                        </form>
+                    </div>
+                    <?php } ?>
+                    <form method="POST" class="comment_form">
+                        <textarea name="user_comment" class="user_comment"></textarea>
+                        <button name="post" class="submit">投稿</button>
+                    </form>
+                </div>
+	            <ul class="user_actions">
+                    <li class="action">
+		                <a href="add.php">
+		                    <span class="action_name">
+			                    <img src="<?= IMAGES_DIR ?>/write.png">
+			                    夢を書く
+		                    </span>
+		                </a>
+	                </li>
+	                <li class="action">
+		                <a href="search.php">
+		                    <span class="action_name">
+			                    <img src="<?= IMAGES_DIR ?>/search.png">
+			                    夢を検索する
+		                    </span>
+		                </a>
+	                </li>
+	                <li class="action">
+		                <a href="logout.php">
+		                    <span class="action_name">
+			                    <img src="<?= IMAGES_DIR ?>/logout.png">
+			                    ログアウト
+		                    </span>
+		                </a>
+	                </li>
+	            </ul>
+            </div>
+    </body>
 </html>
